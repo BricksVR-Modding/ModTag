e43ced988677dc9fb2429b96e9751ea169607ba0
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2021-12-20 15:58:21 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,52 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Normal.Realtime;
+using System.Collections;
+namespace ModTag
+{
+    public static class BuildInfo
+    {
+        public const string Name = "ModTag"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Adds a \"Mod Tag\" to the player's name."; // Description for the Mod.  (Set as null if none)
+        public const string Author = "BelugaTheAxolotl#2134"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = "BricksVR Modding"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/BricksVR-Modding/ModTag/releases"; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class ApplyTag : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildindex, string sceneName)
+        {
+            var rt = GameObject.Find("MetaObjects/Realtime").GetComponent<Realtime>();
+            MelonCoroutines.Start(connectionCheck(rt));
+        }
+        public override void OnUpdate()
+        {
+            if(Input.GetKeyDown("u"))
+            {
+                var settings = GameObject.Find("MetaObjects/UserSettings").GetComponent<UserSettings>();
+                if (settings.Nickname.Contains("(MODDED)")) return;
+                settings.Nickname = settings.Nickname+"\n(MODDED)";
+            }
+        }
+        public IEnumerator connectionCheck(Realtime rt)
+        {
+            while(true)
+            {
+                if(rt.connected)
+                {
+                    var settings = GameObject.Find("MetaObjects/UserSettings").GetComponent<UserSettings>();
+                    if (!settings.Nickname.Contains("(MODDED)")) settings.Nickname = settings.Nickname + "\n(MODDED)";
+                }
+                else
+                {
+                    var settings = GameObject.Find("MetaObjects/UserSettings").GetComponent<UserSettings>();
+                    if (settings.Nickname.Contains("(MODDED)")) settings.Nickname = settings.Nickname.Replace("\n(MODDED)", "");
+
+                }
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(ModTag.BuildInfo.Description)]
+[assembly: AssemblyDescription(ModTag.BuildInfo.Description)]
+[assembly: AssemblyCompany(ModTag.BuildInfo.Company)]
+[assembly: AssemblyProduct(ModTag.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + ModTag.BuildInfo.Author)]
+[assembly: AssemblyTrademark(ModTag.BuildInfo.Company)]
+[assembly: AssemblyVersion(ModTag.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(ModTag.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(ModTag.ApplyTag), ModTag.BuildInfo.Name, ModTag.BuildInfo.Version, ModTag.BuildInfo.Author, ModTag.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("BricksVR", "BricksVR")]
